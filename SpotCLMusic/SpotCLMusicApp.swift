48b2158986959c07d64632032b8d2c4415b4a2a2
Syed Talha Rizwan <talharizwan013@gmail.com>
2023-09-11 12:10:13 +0500

    Initial Commit

---
+++ SpotCLMusic/SpotCLMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotCLMusicApp.swift
+//  SpotCLMusic
+//
+//  Created by Rizwan on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotCLMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
